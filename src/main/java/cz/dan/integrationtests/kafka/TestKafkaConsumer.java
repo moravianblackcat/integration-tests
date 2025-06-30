693568163e11c0c1d2d6ddacf5a3ec369abc1ef6
Daniel Polách <polachdan@gmail.com>
2025-06-30 17:14:16 +0200

    TestKafkaConsumer

---
+++ src/main/java/cz/dan/integrationtests/kafka/TestKafkaConsumer.java
@@ -0,0 +1,41 @@
+package cz.dan.integrationtests.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+@Component
+public class TestKafkaConsumer {
+
+    private final Map<String, Integer> topics = new HashMap<>();
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public TestKafkaConsumer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public <T> List<T> getNextMessagesFromTopicWithTimeout(int numberOfMessages, String topic,
+                                                           int timeoutInSeconds) {
+        Integer nextTopicOffset = topics.getOrDefault(topic, 0);
+
+        List<T> result = IntStream.range(0, numberOfMessages)
+                .mapToObj(offset ->
+                        kafkaTemplate.receive(topic, 0, offset, Duration.of(timeoutInSeconds, SECONDS))
+                                .value())
+                .map(message -> (T) message)
+                .toList();
+
+        topics.put(topic, nextTopicOffset + numberOfMessages);
+
+        return result;
+    }
+
+}
