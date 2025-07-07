60aa42063dd5a402ff53864e5ee19b4e7b3abd46
Daniel Polách <polachdan@gmail.com>
2025-07-07 11:01:07 +0200

    HttpResponsesCache to handle LocalDates

--- src/main/java/cz/dan/integrationtests/http/HttpResponsesCache.java
+++ src/main/java/cz/dan/integrationtests/http/HttpResponsesCache.java
@@ -1,8 +1,12 @@
 package cz.dan.integrationtests.http;
 
+import java.time.LocalDate;
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class HttpResponsesCache extends HashMap<String, Object> {
+
+    private static final Pattern LOCAL_DATE_PATTERN = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
 
     @SuppressWarnings("unchecked")
     public List<Map<String, Object>> getArray(String key) {
@@ -24,10 +28,59 @@ public class HttpResponsesCache extends HashMap<String, Object> {
 
     @Override
     public Object put(String key, Object value) {
-        if (!(value instanceof Map || value instanceof List)) {
+        if (!isMapOrList(value)) {
             throw new IllegalArgumentException("Value must be Map<String, Object> or List<Map<String, Object>>");
         }
-        return super.put(key, value);
+        Object converted = convert(value);
+        return super.put(key, converted);
     }
 
+    private boolean isMapOrList(Object v) {
+        return v instanceof Map || v instanceof List;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Object convert(Object value) {
+        if (value instanceof Map) {
+            return convert((Map<String, Object>) value);
+        } else if (value instanceof List) {
+            return convert((List<Object>) value);
+        } else if (isLocalDateString(value)) {
+            return LocalDate.parse((String) value);
+        }
+        return value;
+    }
+
+    private Map<String, Object> convert(Map<String, Object> value) {
+        Map<String, Object> result = new LinkedHashMap<>();
+        for (Entry<String, Object> entry : value.entrySet()) {
+            Object v = entry.getValue();
+            if (isLocalDateString(v)) {
+                result.put(entry.getKey(), LocalDate.parse((String) v));
+            } else if (isMapOrList(v)) {
+                result.put(entry.getKey(), convert(v));
+            } else {
+                result.put(entry.getKey(), v);
+            }
+        }
+        return result;
+    }
+
+    private List<Object> convert(List<Object> value) {
+        List<Object> result = new ArrayList<>();
+        for (Object v : value) {
+            if (isLocalDateString(v)) {
+                result.add(LocalDate.parse((String) v));
+            } else if (isMapOrList(v)) {
+                result.add(convert(v));
+            } else {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+
+    private boolean isLocalDateString(Object value) {
+        return value instanceof String && LOCAL_DATE_PATTERN.matcher((String) value).matches();
+    }
 }
